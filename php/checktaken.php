a434c6229ed0e0a438a31ceb9101646bb59e2e5e
Gabriel Vogt <gvogt@asu.edu>
2017-11-26 09:01:20 -0800

    Tabs to spaces

--- php/checktaken.php
+++ php/checktaken.php
@@ -1,58 +1,58 @@
 <?php
 
-	include 'connect.php';
+    include 'connect.php';
 
-	if (isset($_POST['submit']))
-	{
+    if (isset($_POST['submit']))
+    {
 
-		# New Array
-		$user = array();
-		$user[0] = $_POST["firstName"];
-		$user[1] = $_POST["lastName"];
-		$user[2] = $_POST["userName"];
-		$user[3] = $_POST["password"];
-		$user[4] = $_POST["email"];
-		$user[5] = $_POST["comments"];
+        # New Array
+        $user = array();
+        $user[0] = $_POST["firstName"];
+        $user[1] = $_POST["lastName"];
+        $user[2] = $_POST["userName"];
+        $user[3] = $_POST["password"];
+        $user[4] = $_POST["email"];
+        $user[5] = $_POST["comments"];
 
-	    # Encrypt user's pass
-	    $hash = crypt('$user[3]');
-	    $user[3] = $hash;
+        # Encrypt user's pass
+        $hash = crypt('$user[3]');
+        $user[3] = $hash;
 
-		# Prepare and excludee duplicate username 
-		$stmt = $db->prepare("SELECT userName FROM userinfo WHERE userName = :userName");
-		$stmt->bindParam(':userName', $user[2]);
-		$stmt->execute();
+        # Prepare and excludee duplicate username 
+        $stmt = $db->prepare("SELECT userName FROM userinfo WHERE userName = :userName");
+        $stmt->bindParam(':userName', $user[2]);
+        $stmt->execute();
 
 
-		# Check for duplicate username entry
-		if ($stmt->rowCount() > 0)
-		{
-			echo "Please choose another username";
-		}
-		else
-		{
+        # Check for duplicate username entry
+        if ($stmt->rowCount() > 0)
+        {
+            echo "Please choose another username";
+        }
+        else
+        {
 
-			# Note to test SQL Injections
-			$stmt = $db->prepare("INSERT INTO `userinfo` (`firstName`, `lastName`, `userName`, `password`, `email`, `comments`) VALUES (:firstName, :lastName, :userName, :password, :email, :comments)");		
+            # Note to test SQL Injections
+            $stmt = $db->prepare("INSERT INTO `userinfo` (`firstName`, `lastName`, `userName`, `password`, `email`, `comments`) VALUES (:firstName, :lastName, :userName, :password, :email, :comments)");        
 
-			# Parameter, Value
-		    $stmt->bindValue(':firstName', $user[0]);
-		    $stmt->bindValue(':lastName', $user[1]);
-		    $stmt->bindValue(':userName', $user[2]);
-		    $stmt->bindValue(':password', $user[3]);
-		    $stmt->bindValue(':email', $user[4]);
-		    $stmt->bindValue(':comments', $user[5]);	
+            # Parameter, Value
+            $stmt->bindValue(':firstName', $user[0]);
+            $stmt->bindValue(':lastName', $user[1]);
+            $stmt->bindValue(':userName', $user[2]);
+            $stmt->bindValue(':password', $user[3]);
+            $stmt->bindValue(':email', $user[4]);
+            $stmt->bindValue(':comments', $user[5]);    
 
-			# The prepared statement
-			$stmt->execute();			
+            # The prepared statement
+            $stmt->execute();            
 
-			# Redirect to thank you if sucessful
-			header("Location:/mySite/php/thankyou.php");
-		}
-	}
-	else
-	{
-		echo 'Error in design!'; # Note to redesign
-	}
+            # Redirect to thank you if sucessful
+            header("Location:/mySite/php/thankyou.php");
+        }
+    }
+    else
+    {
+        echo 'Error in design!'; # Note to redesign
+    }
 
 ?>
