999b5aadb51efe857dc1cfa45afc85ab2ad92046
Gabriel Vogt <gvogt@asu.edu>
2017-11-26 05:32:13 -0800

    Added a login
    updated dynamic headers and footers on pages. need to make it not bleed into the content

--- php/header.php
+++ php/header.php
@@ -11,6 +11,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="php/signup.php">Sign Up</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="php/login.php">Login</a>
+            </li>            
         </ul>
     </div>
 </nav>
--- php/signup.php
+++ php/signup.php
@@ -13,99 +13,93 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script> 
+            $(function(){
+              $("#header").load("php/header.php"); 
+              $("#footer").load("php/footer.php"); 
+            });
+        </script>         
     </head>
     <body>
         <?php include('connect.php') ?>
-        <nav class="navbar-inverse navbar-toggleable-md navbar-light bg-faded">
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <a class="navbar-brand" href="#">Gabriel Vogt's</a>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="php/signup.php">Sign Up</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 col-md-6 col-lg-6 float-left">
-                    <form class="form" method="post" action="php/insert.php">
-                        <div class="form-horizontal">
-                            <label>First Name</label>
-                            <div>
-                                <input name="firstName" class="form-control mx-sm-4">
+        <div class="wrapper">
+        <div id="header"></div>        
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-6 col-lg-6 float-left">
+                        <form class="form" method="post" action="php/insert.php">
+                            <div class="form-horizontal">
+                                <label>First Name</label>
+                                <div>
+                                    <input name="firstName" class="form-control mx-sm-4">
+                                </div>
+                                <small id="fNameHelp" class="text-muted" >
+                                Enter your first name!
+                                </small> 
                             </div>
-                            <small id="fNameHelp" class="text-muted" >
-                            Enter your first name!
-                            </small> 
-                        </div>
-                        <br>
-                        <div class="form-horizontal">
-                            <label>Last Name</label>
-                            <div>
-                                <input name="lastName" class="form-control mx-sm-4">                                
+                            <br>
+                            <div class="form-horizontal">
+                                <label>Last Name</label>
+                                <div>
+                                    <input name="lastName" class="form-control mx-sm-4">                                
+                                </div>
+                                <small id="LNameHelp" class="text-muted">
+                                Enter your last name!
+                                </small>                                                                                            
                             </div>
-                            <small id="LNameHelp" class="text-muted">
-                            Enter your last name!
-                            </small>                                                                                            
-                        </div>
-                        <br>                        
-                        <div class="form-horizontal">
-                            <label>User Name</label>
-                            <div>
-                                <input name="userName" class="form-control mx-sm-4">                                
+                            <br>                        
+                            <div class="form-horizontal">
+                                <label>User Name</label>
+                                <div>
+                                    <input name="userName" class="form-control mx-sm-4">                                
+                                </div>
+                                <small id="uNameHelp" class="text-muted">
+                                Enter your user name!
+                                </small>                             
                             </div>
-                            <small id="uNameHelp" class="text-muted">
-                            Enter your user name!
-                            </small>                             
-                        </div>
-                        <br>                        
-                        <div class="form-horizontal">
-                            <label>Password</label>
-                            <div>
-                                <input name="password" type="password" class="form-control mx-sm-4">                                
+                            <br>                        
+                            <div class="form-horizontal">
+                                <label>Password</label>
+                                <div>
+                                    <input name="password" type="password" class="form-control mx-sm-4">                                
+                                </div>
+                                <small id="passHelp" class="text-muted">
+                                Password must consist of one uppercase, one number and one special character!
+                                </small>                            
                             </div>
-                            <small id="passHelp" class="text-muted">
-                            Password must consist of one uppercase, one number and one special character!
-                            </small>                            
-                        </div>
-                        <br>                        
-                        <div class="form-horizontal">
-                            <label>Email Address</label>
-                            <div>
-                                <input name="email" type="email" class="form-control mx-sm-4">                                
+                            <br>                        
+                            <div class="form-horizontal">
+                                <label>Email Address</label>
+                                <div>
+                                    <input name="email" type="email" class="form-control mx-sm-4">                                
+                                </div>
+                                <small id="emailHelp" class="text-muted">
+                                No xyz!
+                                </small>                             
                             </div>
-                            <small id="emailHelp" class="text-muted">
-                            No xyz!
-                            </small>                             
-                        </div>
-                        <br>                        
-                        <div class="form-horizontal">
-                            <label>Comments</label>
-                            <div>
-                                <input name="comments" class="form-control mx-sm-4">                                
+                            <br>                        
+                            <div class="form-horizontal">
+                                <label>Comments</label>
+                                <div>
+                                    <input name="comments" class="form-control mx-sm-4">                                
+                                </div>
+                                <small id="cHelp" class="text-muted">
+                                Any additional comments
+                                </small>                          
                             </div>
-                            <small id="cHelp" class="text-muted">
-                            Any additional comments
-                            </small>                          
+                            <br>                        
+                            <button class="btn btn-default" name="submit" type="submit" value="Submit">Submit</button>
+                            <button class="btn btn-default" type="reset" value="Reset">Clear</button>                        
+                        </form>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+                        <div class="item">
+                            <img src="images/puppy.jpg" alt="Photo of a puppy!" style="width:100%;">
                         </div>
-                        <br>                        
-                        <button class="btn btn-default" name="submit" type="submit" value="Submit">Submit</button>
-                        <button class="btn btn-default" type="reset" value="Reset">Clear</button>                        
-                    </form>
-                </div>
-                <div class="col-sm-6 col-md-6 col-lg-6">
-                    <div class="item">
-                        <img src="images/puppy.jpg" alt="Photo of a puppy!" style="width:100%;">
                     </div>
                 </div>
             </div>
         </div>
+        <div id="footer" class="footer"></div>
     </body>
 </html>
